45638c03f2874f68e437e932abb6ae77d3c2c5aa
Rebekah <joeun514@gmail.com>
2023-04-11 13:44:27 -0500

    .

---
+++ lib/learn_earn_main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:totem_test_app/learn_box.dart';
+import 'package:totem_test_app/quiz_of_the_day.dart';
+
+class LearnEarnMain extends StatefulWidget {
+  const LearnEarnMain({
+    super.key,
+  });
+
+  @override
+  State<LearnEarnMain> createState() => _LearnEarnMainState();
+}
+
+class _LearnEarnMainState extends State<LearnEarnMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text(
+          "Read through each mini presentation and take a quiz to earn money. Happy learning!",
+          style: TextStyle(color: Colors.white, fontSize: 14),
+        ),
+        const SizedBox(height: 40),
+        const Text(
+          "QUIZ OF THE DAY!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'Gothic A1',
+          ),
+        ),
+        const SizedBox(height: 16),
+        const QuizOfTheDay(
+          dollarAmount: 1.00,
+          time: 5,
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        const Text(
+          'TRIBES',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'Gothic A1',
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Row(
+          children: [
+            LearnBox(
+              title: 'Origins of Choctaw Nation',
+              earnAmount: 400,
+              estTime: 3,
+              displayImage: Image.asset('assets/images/Choctaw_seal.png'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,152 @@
 import 'package:flutter/material.dart';
-import 'package:totem_test_app/learn_earn.dart';
-import 'package:totem_test_app/search.dart';
+import 'package:go_router/go_router.dart';
+import 'package:totem_test_app/deals_and_offers.dart';
+import 'package:totem_test_app/learn_earn_main.dart';
+import 'package:totem_test_app/explore_tribes.dart';
+
+import 'donations_main.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
     );
   }
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const GenericPage(
+          title: "Main page",
+          child: Placeholder(),
+        ),
+      ),
+      GoRoute(
+        path: '/deals',
+        builder: (context, state) => const GenericPage(
+          title: "Totem deals",
+          child: DealsAndOffers(),
+        ),
+      ),
+      GoRoute(
+        path: '/learn_earn',
+        builder: (context, state) => const GenericPage(
+          title: "Learn & Earn",
+          child: LearnEarnMain(),
+        ),
+      ),
+      GoRoute(
+        path: '/donations',
+        builder: (context, state) => const GenericPage(
+          title: "Donation",
+          child: DonationsMain(),
+        ),
+      ),
+      GoRoute(
+        path: '/explore_tribes',
+        builder: (context, state) => const GenericPage(
+          title: "Explore Tribes",
+          child:
+              ExploreTribes(), // add this line to render the ExploreTribes widget
+        ),
+      ),
+    ],
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
+class GenericPage extends StatefulWidget {
+  const GenericPage({
     super.key,
+    required this.title,
+    required this.child,
   });
 
+  final String title;
+  final Widget child;
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GenericPage> createState() => _GenericPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _GenericPageState extends State<GenericPage> {
+  static int _currentIndex = 0;
+  static const _routes = [
+    _NavigationItems(
+      route: '/',
+      icon: Icons.person,
+      label: 'Account',
+    ),
+    _NavigationItems(
+      route: '/deals',
+      icon: Icons.discount,
+      label: 'Deals',
+    ),
+    _NavigationItems(
+      route: '/donations',
+      icon: Icons.favorite,
+      label: 'Donations',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Totem Deals"),
+        title: Text(widget.title),
         elevation: 0,
         backgroundColor: Colors.black,
       ),
       backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.black,
+        onTap: (newIndex) {
+          if (_currentIndex == newIndex) return;
+          setState(() {
+            _currentIndex = newIndex;
+            context.go(_routes[newIndex].route);
+          });
+        },
+        items: [
+          for (final item in _routes)
+            BottomNavigationBarItem(
+              icon: Icon(item.icon),
+              label: item.label,
+            ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [
-              SearchBox(),
-              SizedBox(height: 45),
-              LearnEarn(),
-              SizedBox(height: 45),
-              Text(
-                "DEALS & OFFERS",
-                style: TextStyle(
-                  color: Color(0xff4f4d4d),
-                  fontSize: 12,
-                ),
-              ),
-            ],
-          ),
+          child: widget.child,
         ),
       ),
     );
   }
+}
+
+class _NavigationItems {
+  const _NavigationItems({
+    required this.route,
+    required this.icon,
+    required this.label,
+  });
+
+  final String route;
+  final IconData icon;
+  final String label;
 }
